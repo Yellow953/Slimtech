8a054a666f7356b0a43de86bd6891e9b8b2b1c3f
Yellow <joemazloum953@gmail.com>
2024-01-02 08:47:42 +0100

    link to yellowtech

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,11 @@
 <!-- Footer Start -->
-<div class="footer container-fluid mt-5 py-5 px-sm-3 px-md-5 text-white text-center" style="background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+<div class="footer container-fluid mt-5 py-5 px-sm-3 px-md-5 text-white text-center"
+    style="background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
 url({{asset('assets/images/logo.png')}});  background-size:contain; background-position:center; background-repeat:no-repeat;">
     <div class="container border-top border-dark pt-5">
         <p class="m-0 text-center text-white">
             &copy; SlimTech, All Rights Reserved. Designed
-            by YellowTech
+            by <a href="https://yellowtech.dev" target="_blank">YellowTech</a>
         </p>
     </div>
 </div>
